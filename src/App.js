dfa33903733c90de245c48d454323d1a9e8eebbd
dkdkwl <dkdkwl@naver.com>
2022-07-09 14:58:06 +0900

    라우터 해보기

--- src/App.js
+++ src/App.js
@@ -2,35 +2,41 @@ import './App.css';
 import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 import { useState } from 'react';
 import data from './data.js';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
-
 
   let [shoes] = useState(data)
   
    return (
     <div className="App">
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">cart</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+      <Routes>
+        <Route path="/" element ={
+          <div className="container">
+            <div className='row'>
+              {shoes.map( (a,i)=>{
+                  return(
+                    <Items i={i+1} shoes={shoes[i]} />
+                  )
+                })}
+            </div>
+          </div>
+        }/>
+        <Route path="/detail" element ={<Detail/>}/>
+        <Route path="/abput" element={<div>어바웃페이지임</div>}/>
+      </Routes>
 
-        <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">cart</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      <div className="container">
-        <div className='row'>
-          {
-            shoes.map( (a,i)=>{
-              return(
-                <Items i={i+1} shoes={shoes[i]} />
-              )
-            })
-          }
-        </div>
-      </div>
+      <Link to="/">HOME</Link>
+      <Link to="/detail">상세페이지</Link>
     </div>
   );
 }
@@ -46,4 +52,12 @@ function Items(props){
   )
 }
 
+
+function Detail(){
+  return(
+    <>
+
+  </>
+  )
+}
 export default App;
