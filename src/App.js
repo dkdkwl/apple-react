721b9870eb31a92508f5c0880018fe0fd12c1cf5
dkdkwl <dkdkwl@naver.com>
2022-07-09 15:27:24 +0900

    라우터 이벤트페이지 만들기

--- src/App.js
+++ src/App.js
@@ -2,23 +2,29 @@ import './App.css';
 import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 import { useState } from 'react';
 import data from './data.js';
-import { Routes, Route, Link } from 'react-router-dom';
+import Detail from './routes/Detail.js';
+import { Routes, Route, Link , useNavigate, Outlet} from 'react-router-dom';
 
 function App() {
 
   let [shoes] = useState(data)
-  
+  let navigate = useNavigate();
+
    return (
     <div className="App">
-    <Navbar bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">cart</Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link onClick={ ()=>{ navigate('/')} }>Home</Nav.Link>
+            <Nav.Link onClick={ ()=>{ navigate(1)} }>앞으로</Nav.Link>
+            <Nav.Link onClick={ ()=>{ navigate(-1)} }>뒤로</Nav.Link>
+            <Nav.Link onClick={ ()=>{ navigate('detail')} }>detail</Nav.Link>
+          <Link to="/">HOME</Link>
+          <Link to="/detail">상세페이지</Link>
+          </Nav>
+        </Container>
+      </Navbar>
       <Routes>
         <Route path="/" element ={
           <div className="container">
@@ -32,13 +38,36 @@ function App() {
           </div>
         }/>
         <Route path="/detail" element ={<Detail/>}/>
-        <Route path="/abput" element={<div>어바웃페이지임</div>}/>
+        <Route path="/about" element={<About/>}>
+          <Route path="member" element={<div>멤버임</div>}/>
+          <Route path="location" element={<div>위치정보임</div>}/>
+        </Route>
+        <Route path="/event" element={<EventPage/>}>
+            <Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>}></Route>
+            <Route path="two" element={<p>생일기념 쿠폰받기</p>}></Route>
+          </Route>
       </Routes>
-
-      <Link to="/">HOME</Link>
-      <Link to="/detail">상세페이지</Link>
     </div>
   );
+}
+
+function About(){
+  return(
+    <div>
+      <h4>회사정보임</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+
+function EventPage(){
+  return(
+    <div>
+      <h4>이벤트페이지</h4>
+      <Outlet></Outlet>
+    </div>
+  )
 }
 
 
@@ -53,11 +82,4 @@ function Items(props){
 }
 
 
-function Detail(){
-  return(
-    <>
-
-  </>
-  )
-}
 export default App;
