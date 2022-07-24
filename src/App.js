1a8de4d9e9cd7b54a9b3ff5d748616661ae9157c
dkdkwl <dkdkwl@naver.com>
2022-07-24 11:26:27 +0900

    ajax로 실시간 변경하기

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { useState } from 'react';
 import data from './data.js';
 import Detail from './routes/Detail.js';
 import { Routes, Route, Link , useNavigate, Outlet} from 'react-router-dom';
+import axios from 'axios';
+
 
 function App() {
-
-  let [shoes] = useState(data)
+  
+  let [shoes,setShoes] = useState(data)
   let navigate = useNavigate();
 
    return (
@@ -31,21 +33,30 @@ function App() {
             <div className='row'>
               {shoes.map( (a,i)=>{
                   return(
-                    <Items i={i+1} shoes={shoes[i]} />
+                    <Items i={i+1} shoes={shoes[i]} key={i} />
                   )
                 })}
+
+                <button onClick={()=>{
+                  axios.get('https://codingapple1.github.io/shop/data2.json')
+                  .then(
+                    (결과)=>{
+                      let copy = [...shoes,...결과.data];
+                      setShoes(copy);
+                    })
+                }}>버튼버튼</button>
             </div>
           </div>
         }/>
-        <Route path="/detail" element ={<Detail/>}/>
         <Route path="/about" element={<About/>}>
           <Route path="member" element={<div>멤버임</div>}/>
           <Route path="location" element={<div>위치정보임</div>}/>
         </Route>
         <Route path="/event" element={<EventPage/>}>
-            <Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>}></Route>
-            <Route path="two" element={<p>생일기념 쿠폰받기</p>}></Route>
-          </Route>
+          <Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>}></Route>
+          <Route path="two" element={<p>생일기념 쿠폰받기</p>}></Route>
+        </Route>
+          <Route path="/detail/:id" element ={<Detail shoes={shoes} />}/>
       </Routes>
     </div>
   );
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,14 +1,39 @@
-function Detail(){
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function Detail(props){
+    
+    let {id} = useParams();
+    let [count,setCount] = useState(0);
+    // let [alert,setAlert] = useState(true);
+    let [num,setNum] = useState("");
+    let 찾은상품 = props.shoes.find(function(x){
+        return x.id == id;
+    });
+
+
+
     return(
+
         <div className="container">
+            {/* {alert == true 
+            ? <div className="alert alert-warning">
+                2초이내 구매시 할인
+            </div> 
+            : null} */}
+            {count}
+            <button onClick={()=>{
+                setCount(++count);
+            }}>숫자증가 시키기</button>
             <div className="row">
                 <div className="col-md-6">
                 <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
                 </div>
                 <div className="col-md-6">
-                <h4 className="pt-5">상품명</h4>
-                <p>상품설명</p>
-                <p>120000원</p>
+                    <input className="useInput" type="text" onChange = {((e)=>{ setNum(e.target.value) })} />
+                <h4 className="pt-5">{찾은상품.title}</h4>
+                <p>{찾은상품.content}</p>
+                <p>{찾은상품.price} 원</p>
                 <button className="btn btn-danger">주문하기</button> 
                 </div>
             </div>
